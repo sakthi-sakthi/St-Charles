836965d2068f553206ecbfc60555cb771cbbe9bf
preethi-09 <preethicharles09@gmail.com>
2023-09-30 12:52:04 +0530

    add routes and create home page
    add routes and create home page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import About from './components/about';
-import Churchstatic from './components/churchstatic';
-import Footer from './components/footer';
-import Header from './components/header';
-import Latestevent from './components/latestevent';
-import Ourstatic from './components/ourstatic';
-import Scrollbar from './components/scrollbar';
-import Slider from './components/slider';
-import Testimonials from './components/testimonials';
-import Upcoming from './components/upcoming';
-
+import Home from "./home";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
-      <Slider />
-      <Scrollbar />
-      <About />
-      <Ourstatic />
-      <Upcoming />
-      <Churchstatic />
-      <Testimonials />
-      <Footer />
+     <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
---
+++ src/home.js
@@ -0,0 +1,29 @@
+import About from './components/about';
+import Churchstatic from './components/churchstatic';
+import Footer from './components/footer';
+import Header from './components/header';
+import Latestevent from './components/latestevent';
+import Ourstatic from './components/ourstatic';
+import Scrollbar from './components/scrollbar';
+import Slider from './components/slider';
+import Testimonials from './components/testimonials';
+import Upcoming from './components/upcoming';
+
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Slider />
+      <Scrollbar />
+      <About />
+      <Ourstatic />
+      <Upcoming />
+      <Churchstatic />
+      <Testimonials />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
