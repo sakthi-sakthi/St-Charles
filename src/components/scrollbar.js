057a02dc37bc8d7ccd11dbdc3c7cc7467f91bfbc
Muniraj M <107682409+Muniraj132@users.noreply.github.com>
2023-10-05 17:27:37 +0530

    implement the flash news
    implement the flash news

--- src/components/scrollbar.js
+++ src/components/scrollbar.js
@@ -6,7 +6,13 @@ function Scrollbar() {
       <div className="scrollbar">
         <div className="container">
           <div className="row flex-wrap justify-content-center justify-content-lg-between align-items-lg-center">
-            <div className="col-12 col-lg-12 d-none d-md-flex flex-wrap justify-content-center justify-content-lg-start mb-3 mb-lg-0">
+            <div className="col-12 col-lg-2 ">
+              <label className="buttonflash" style={{ position: "relative" }}>
+                {" "}
+                Flash News
+              </label>
+            </div>
+            <div className="col-12 col-lg-10 d-none d-md-flex flex-wrap justify-content-center justify-content-lg-start mb-3 mb-lg-0">
               <div className="marqueenews">
                 <div className="marquee">
                   <p>
