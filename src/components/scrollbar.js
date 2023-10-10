f76a0e37a4f18bcfd7dc3d5dcecab7c00926cc8f
Muniraj M <107682409+Muniraj132@users.noreply.github.com>
2023-10-10 10:14:16 +0530

    implement the flashnews image
    implement the flashnews image

--- src/components/scrollbar.js
+++ src/components/scrollbar.js
@@ -6,16 +6,23 @@ function Scrollbar() {
       <div className="scrollbar">
         <div className="container">
           <div className="row flex-wrap justify-content-center justify-content-lg-between align-items-lg-center">
-            <div className="col-12 col-lg-2 ">
-              <label className="buttonflash" style={{ position: "relative" }}>
-                {" "}
+            <div className="col-12 col-lg-2 d-flex">
+              {/* <label className="buttonflash" style={{ position: "relative" }}>
                 Flash News
-              </label>
+              </label> */}
+              <div class="label ripple">Flash News</div>
             </div>
             <div className="col-12 col-lg-10 d-none d-md-flex flex-wrap justify-content-center justify-content-lg-start mb-3 mb-lg-0">
               <div className="marqueenews">
                 <div className="marquee">
                   <p>
+                    <img
+                      src="images/logos/output-onlinegiftools.gif"
+                      style={{
+                        maxWidth: "40px",
+                      }}
+                      alt=""
+                    />
                     Welcome to The Sisters of St. Charles Borromeo Eastern
                     province
                   </p>
